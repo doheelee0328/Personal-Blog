2c76664e73d8c736884cf56e9b5b003c53d59093
Dohee Lee <dojuhee2015@gmail.com>
2023-08-12 15:23:05 +0100

    add profile page

--- client/src/pages/EditProfile/ EditProfile.jsx
+++ client/src/pages/EditProfile/ EditProfile.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import img from '../../image/blankProfile.webp'
+import '../../scss/editProfile.scss'
 
 const EditProfile = () => {
   return (
-    <form>
-      <label>Name</label>
-      <input />
-      <label>Email</label>
-      <input />
-      <label>Password</label>
-      <input />
-      <label>Confirm Password</label>
-      <input />
-    </form>
+    <div className='background-profile'>
+      <form className='form-container '>
+        <img src={img} alt='edit-profile' className='edit-profile' />
+        <div className='email-password-container '>
+          <label className='labels'>Name</label>
+          <input type='text' className='inputs' />
+          <label className='labels'>Email</label>
+          <input type='text' className='inputs' />
+          <label className='labels'>Password</label>
+          <input type='text' className='inputs' />
+          <label className='labels'>Confirm Password</label>
+          <input type='text' className='inputs' />
+        </div>
+        <button className='profile-buttons'>Edit Profile</button>
+        <button className='profile-buttons'>Delete Profile</button>
+      </form>
+    </div>
   )
 }
 
