da11aea69cddada92180e88ded8d629773604b31
Dohee Lee <dojuhee2015@gmail.com>
2023-08-22 18:46:03 +0100

    add

--- server/routers/commentRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-const { addComment } = require('../controllers/commentController')
-
-router.post('/', addComment)
-
-module.exports = router
